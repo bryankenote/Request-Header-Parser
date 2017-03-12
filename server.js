ab2f456ff94fab9ed404451aa34ff53c9d10270a
Bryan <brykenote@gmail.com>
2017-03-12 06:32:40 +0000

    parse header and return results

--- server.js
+++ server.js
@@ -3,9 +3,27 @@ let app = express();
 let port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.end('init');
+  let parsed = {
+    ipaddress: getIP(req.connection.remoteAddress)
+  , language: getLanguage(req.headers['accept-language'])
+  , software: getSoftware(req.headers['user-agent'])
+  }
+  res.end(JSON.stringify(parsed));
 });
 
 app.listen(port, function () {
   console.log('Example app listening on port '+port+'!');
 });
+
+function getIP(remoteAddress) {
+  let isV6 = remoteAddress.indexOf(':') >= 0;
+  return isV6 ? remoteAddress.split(':').reverse()[0] : remoteAddress;
+}
+
+function getLanguage(acceptLanguage) {
+  return acceptLanguage.split(',')[0].trim();
+}
+
+function getSoftware(userAgent) {
+  return userAgent.split(/[\(\)]/)[1].trim();
+}
